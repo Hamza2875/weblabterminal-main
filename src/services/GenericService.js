62859ba6ab000924e018dc0d3960f91a798f89b9
Hamza2875 <72712947+Hamza2875@users.noreply.github.com>
2021-01-02 16:35:52 +0500

    ok

--- src/services/GenericService.js
+++ src/services/GenericService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://restapiiifinal.herokuapp.com/api/products";
+axios.defaults.baseURL = "https://restapiiifinal.herokuapp.com/api/";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 class GenericService {
   constructor() {}
